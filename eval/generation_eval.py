42e84d7bc4ee66d435c488c86f44c329a2804b0b
Parker Mina <minap@wwu.edu>
2025-05-23 20:38:56 -0700

    Added calculations for Retrieval and Generation evaluation metrics

--- eval/generation_eval.py
+++ eval/generation_eval.py
@@ -1,3 +1,32 @@
 """
 Implementation of the generation evaluation metrics.
 """
+
+import evaluate
+import pandas as pd
+from rag import get_context_prompt, get_embedding, searck_kb
+from models import query_model
+
+def generation_eval(predictions, references):
+    df = pd.read_csv('eval.csv')
+    for question, long_answer in zip(df['question'], df['long_answer']):
+        prompt_embedding = get_embedding(question)
+        results = searck_kb(prompt_embedding, top_k=3)
+        context_prompt = get_context_prompt(question, results)
+        title, response = query_model(context_prompt)
+
+        rouge = evaluate.load("rouge")
+        bleu = evaluate.load("bleu")
+        bertscore = evaluate.load("bertscore")
+
+        results_rouge = rouge.compute(predictions=question, references=long_answer)
+        print(results_rouge)
+
+        tokenized_preds = [pred.split() for pred in question]
+        tokenized_refs = [[ref.split()] for ref in long_answer]
+
+        results_bleu = bleu.compute(predictions=tokenized_preds, references=tokenized_refs)
+        print(results_bleu)
+
+        results_bertscore = bertscore.compute(predictions=question, references=long_answer, lang="en")
+        print(results_bertscore)
--- eval/retrieval_eval.py
+++ eval/retrieval_eval.py
@@ -1,3 +1,26 @@
 """
 Implementation of the RAG retrieval evaluation metrics.
 """
+import evaluate
+import pandas as pd
+from rag import get_context_prompt, get_embedding, searck_kb
+
+def retrieval_eval(predictions, references):
+    df = pd.read_csv('eval.csv')
+    for question, long_answer in zip(df['question'], df['long_answer']):
+        prompt_embedding = get_embedding(question)
+        results = searck_kb(prompt_embedding, top_k=3)
+        print(compute_recall_at_k(results, long_answer))
+        print(compute_precison_at_k(results, long_answer))
+        print(compute_mrr(results, long_answer))
+
+def compute_recall_at_k(retrieved_chunks, gold_answer):
+    return int(any(gold_answer.lower() in chunk.lower() for chunk in retrieved_chunks))
+def compute_precison_at_k(retrieved_chunks, gold_answer):
+    matches = sum(gold_answer.lower() in chunk.lower() for chunk in retrieved_chunks)
+    return matches / len(retrieved_chunks)
+def compute_mrr(retrieved_chunks, gold_answer):
+    for i, chunk in enumerate(retrieved_chunks):
+        if gold_answer.lower() in chunk.lower():
+            return 1 / (i + 1)
+    return 0.0
